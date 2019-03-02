6560531e158ca60c25ea7f083df88db6152c53a3
onehao <onewanghao@gmail.com>
2019-03-02 17:28:59 +0800

    Initial Commit

---
+++ TabNaviDemo/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  TabNaviDemo
+//
+//  Created by wanghao on 2019/3/2.
+//  Copyright © 2019 wanghao. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ TabNaviDemo/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TabNaviDemo
+//
+//  Created by wanghao on 2019/3/2.
+//  Copyright © 2019 wanghao. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
